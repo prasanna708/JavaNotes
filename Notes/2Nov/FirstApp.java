9c8573fcd14e169d9577108c6cc315c916f98c5e
prasannakumar4301 <prasannakumar4301@gmail.com>
2024-07-02 20:34:05 +0530

    v1

---
+++ Notes/2Nov/FirstApp.java
@@ -0,0 +1,10 @@
+class FirstApp{
+  static public void main(String args[]){
+       //System.out.println("Hello!");
+       System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));
+    }
+}
+
+
+// Data - 10
+// Class name should always starts with Uppercase
---
+++ Notes/7Dec/M.java
@@ -0,0 +1,9 @@
+public class Dec{
+    public static void main(String args[]){
+        System.out.println("December!");
+    }
+
+   private void m1(){
+
+    }
+}
